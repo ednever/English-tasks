081fa68f3cc68fb1bf72e5274a53ba72f092b7fb
Эдгар Неверовский <edgarneverovskij@gmail.com>
2021-12-15 16:23:30 +0200

    New

---
+++ ExercisesPart1/ExercisesPart1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Login_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //login: admin
+            //password: pass1234
+
+            //declare variables
+            //assign value
+            string login = "admin";
+            string password = "pass1234";
+
+            string userName, userPassword;
+            Console.WriteLine("Enter your login: ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter your password: ");
+            userPassword = Console.ReadLine();
+
+            if (login == userName and password == userPassword)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Oops. Something went wrong.");
+            }
+        }
+    }
+}
